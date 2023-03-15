d76e4fe4ef4140375484efd08055ba9b7a141ff7
Lei Sun <sunleibest@qq.com>
2023-03-15 10:34:55 +0800

    commit test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,39 @@ module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
     themeConfig: {
+        // logo: '/assets/img/logo.png',
         nav: [
           { text: 'Home', link: '/' },
           { text: 'Guide', link: '/guide/' },
+          { text: 'Test', link: '/test/' },
           { text: 'External', link: 'https://google.com' },
         ],
-        sidebar: 'auto'
+        // sidebar: 'auto'
+        // displayAllHeaders: true, // 默认值：false,
+
+        sidebar: [
+          {
+            title: 'Group 1',   // 必要的
+            path: '/guide/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
+            children: [
+              '/',
+              '/test/'
+            ]
+          },
+          {
+            title: 'Group 2',
+            path: '/test/',
+            children: [ /* ... */ ],
+            initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+          },
+          {
+            title: 'Group 3',
+            children: [ /* ... */ ],
+            initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+          }
+        ]
     
       }
   }
--- docs/guide/README.md
+++ docs/guide/README.md
@@ -1,13 +1,56 @@
----
+<!-- ---
 sidebar: auto
----
+--- -->
 
 
 # This is guide page1
-dfsaf
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+## This is guide page11
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+### This is guide page111
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
 
-# This is guide page2
-sdfds
+## This is guide page22
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+### This is guide page222
+aaa
+aaa
+aaa
+aaa
+aaa
+aaa
+## This is guide page33
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+### This is guide page333
 
-# This is guide page3
-fdasdf
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
+aaa<br />
